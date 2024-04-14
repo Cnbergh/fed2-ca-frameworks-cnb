c3bf953fc63c25d13b456f34e544ebc97769e498
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2024-04-14 13:43:49 +0200

    last cleanup?

--- components/productCard.tsx
+++ components/productCard.tsx
@@ -1,9 +1,8 @@
-'use client';
+
 import React, { useState } from 'react';
-import Link from 'next/link';
 import Modal from '@/components/modal';
 import useCart from '@/app/cart/useCart';
-import PageLoader from 'next/dist/client/page-loader';
+import { useRouter } from 'next/navigation';
 
 export function ProductCard({ product }: any) {
   const {
@@ -13,10 +12,12 @@ export function ProductCard({ product }: any) {
     image = {},
     reviews,
     rating,
+    id
   } = product || {};
   const { url = '/FallbackImage.png', alt = 'Default image' } = image;
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { addItemToCart } = useCart();
+  const router = useRouter();
 
   const handleAddToCart = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
@@ -34,54 +35,57 @@ export function ProductCard({ product }: any) {
     setIsModalOpen(false);
   };
 
+  const handleViewDetails = () => {
+    router.push(`/products/${id}`);
+    handleModalOpen();
+  };
+
   return (
     <div className="product-card border border-gray-300 rounded-lg w-full h-full">
-  <img
-    src={url}
-    alt={alt}
-    onClick={handleModalOpen}
-    className="w-full h-72 max-h-96 object-cover mb-4 rounded-lg"
-  />
-  <div className="border border-gray-300 p-4 rounded-b-lg">
-    <h3 className="mb-2">{title}</h3>
-    <p className="mb-4">{description}</p>
-    <p className="mb-2">Rating: {rating}</p>
-    <button
-      onClick={handleAddToCart}
-      className="bg-gray-300 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-200"
-    >
-      Add to Cart
-    </button>
-  </div>
-  <div className="w-full p-4 mx-auto flex flex-row align-bottom mt-auto">
-    <Link
-      href={`/products/${product.id}`}
-      onClick={PageLoader}
-      className="bg-black hover:bg text-white text-center px-4 py-2 rounded-md w-full"
-    >
-      <span className="mx-auto">View Details</span>
-    </Link>
-  </div>
-  <Modal show={isModalOpen} onClose={handleModalClose}>
-    <div className="modal-content">
       <img
         src={url}
         alt={alt}
-        className="w-full max-h-96 object-cover mb-4 rounded-lg"
+        onClick={handleModalOpen}
+        className="w-full h-72 max-h-96 object-cover mb-4 rounded-lg"
       />
-      <h2 className="text-2xl font-bold mb-2">{title}</h2>
-      <p className="text-gray-600 mb-4">{description}</p>
-      <p className="text-gray-600">Price: ${price}</p>
-      <p className="text-gray-600">Rating: {rating}</p>
-      <button
-        onClick={handleModalClose}
-        className="hover:bg-gray-200 bg-gray-300 text-gray-800 px-4 py-2 rounded-md mt-4"
-      >
-        Close
-      </button>
+      <div className="border border-gray-300 p-4 rounded-b-lg">
+        <h3 className="mb-2">{title}</h3>
+        <p className="mb-4">{description}</p>
+        <p className="mb-2">Rating: {rating}</p>
+        <button
+          onClick={handleAddToCart}
+          className="bg-gray-300 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-200"
+        >
+          Add to Cart
+        </button>
+      </div>
+      <div className="w-full p-4 mx-auto flex flex-row align-bottom mt-auto">
+        <button
+          onClick={handleViewDetails}
+          className="bg-black hover:bg text-white text-center px-4 py-2 rounded-md w-full"
+        >
+          <span className="mx-auto">View Details</span>
+        </button>
+      </div>
+      <Modal show={isModalOpen} onClose={handleModalClose}>
+        <div className="modal-content">
+          <img
+            src={url}
+            alt={alt}
+            className="w-full max-h-96 object-cover mb-4 rounded-lg"
+          />
+          <h2 className="text-2xl font-bold mb-2">{title}</h2>
+          <p className="text-gray-600 mb-4">{description}</p>
+          <p className="text-gray-600">Price: ${price}</p>
+          <p className="text-gray-600">Rating: {rating}</p>
+          <button
+            onClick={handleModalClose}
+            className="hover:bg-gray-200 bg-gray-300 text-gray-800 px-4 py-2 rounded-md mt-4"
+          >
+            Close
+          </button>
+        </div>
+      </Modal>
     </div>
-  </Modal>
-</div>
-
   );
 }
