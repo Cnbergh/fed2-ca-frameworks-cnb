46c5ab927bba90e04ed0c8afbbef64fa3cc54fe0
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2024-04-14 13:11:56 +0200

    omg fuck that bugg

--- app/products/@modal/(..)products/[id]/page.tsx
+++ app/products/@modal/(..)products/[id]/page.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import Modal from '@/components/modal';
 import { ProductCard } from '@/components/productCard';
-import { Product } from '@/app/products/page';
-
-export interface ProductModalProps {
-  product?: Product;
-  productId?: string;
-  onClose: () => void;
-}
-
+import { ProductModalProps } from '@/lib/utils';
 
 const ProductModal: React.FC<ProductModalProps> = ({ product, productId, onClose }) => {
   const fetchProductDetails = async () => {
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export interface ProductModalProps {
+	product?: Product;
+	productId?: string;
+	onClose: () => void;
+  }
+
+
+export function stringOrUndefined(value: string | string[] | undefined): string | undefined {
+	return value === undefined ? undefined : Array.isArray(value) ? value[0] : value.toString();
+  }
